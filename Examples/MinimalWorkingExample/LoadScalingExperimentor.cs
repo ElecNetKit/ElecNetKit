caf3d5feff0a9eb24f5d891d9b53f4070c3470cc
Fabian Tamp <fabian@capnfabs.net>
2013-01-09 09:01:50 +1100

    Added introductory documentation and minimal working examle.

---
+++ Examples/MinimalWorkingExample/LoadScalingExperimentor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ElecNetKit.Experimentation;
+
+namespace MinimalWorkingExample
+{
+    class LoadScalingExperimentor : IExperimentor
+    {
+        public double LoadScalingFactor { set; get; }
+
+        //Convenience constructor, to set scaling factor when object is initialised.
+        public LoadScalingExperimentor(double factor)
+        {
+            LoadScalingFactor = factor;
+        }
+
+        //Implements IExperimentor.Experiment()
+        public List<string> Experiment(ElecNetKit.NetworkModelling.NetworkModel Network)
+        {
+            return Network.Loads.Select(
+                load => String.Format("edit load.{0} kW={1:F6} kVAr={2:F6}",
+                                      load.ID,
+                                      load.ActualKVA.Real * LoadScalingFactor,
+                                      load.ActualKVA.Imaginary * LoadScalingFactor
+                                      )
+                    ).ToList();
+        }
+    }
+}
---
+++ Examples/MinimalWorkingExample/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Usings
+using ElecNetKit.Simulator;         //Contains NetworkController
+using ElecNetKit.Transform;         //Contains DifferenceTransform
+#endregion
+namespace MinimalWorkingExample
+{
+    class Program
+    {
+        #region Main
+        static void Main(string[] args)
+        {
+            //Setup a network controller and choose a network definition file
+            // appropriate to the simulator in use
+            NetworkController controller = new NetworkController();
+            //Set this to the absolute path to the IEEE13mod.dss file, e.g.
+            controller.NetworkFilename = @"C:\temp\IEEE13mod.dss";
+
+            //Add our new experiment, with +10% load scaling.
+            controller.ExperimentDriver = new LoadScalingExperimentor(1.1);
+
+            //Use a DifferenceTransform so that the controller returns the
+            // differences between pre- and post- experiment voltages.
+            controller.ResultsTransformer = new DifferenceTransform();
+
+            //Run the simulation
+            controller.Execute();
+
+            //Output the change in voltage at every network bus.
+            foreach (var bus in controller.Network.Buses.Values)
+            {
+                System.Diagnostics.Debug.WriteLine(
+                "Bus " + bus.ID + " has changed in voltage (pu) by "
+                + bus.VoltagePU.Magnitude);
+            }
+            
+        }
+        #endregion
+    }
+}
