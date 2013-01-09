df6eeb3333109cecd26a441ec6fd1a6e64f5c368
Fabian Tamp <fabian@capnfabs.net>
2013-01-09 11:15:54 +1100

    Added convenient graphing: GraphHost - like VisualHost, except handles automatic updates, and GraphHostWindow, which provides an easy way to launch graphs in a window from console applications.

---
+++ Graphing/Controls/GraphHostWindow.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ElecNetKit.Graphing.Controls
+{
+    public class GraphHostWindow : System.Windows.Window
+    {
+        GraphHost _GraphHost;
+
+        public INetworkGraph Graph
+        {
+            get { return _GraphHost.Graph; }
+            set
+            {
+                _GraphHost.Graph = value;
+            }
+        }
+
+        public GraphHostWindow()
+        {
+            _GraphHost = new GraphHost();
+            this.Content = _GraphHost;
+        }
+
+        private void RefreshGraph()
+        {
+            _GraphHost.RefreshGraph();
+        }
+
+        private static void RunGraphHostWindow(object Graph)
+        {
+            var window = new GraphHostWindow();
+            window.Graph = (INetworkGraph)Graph;
+            window.Show();
+            //See http://stackoverflow.com/questions/4183622/the-calling-thread-must-be-sta-because-many-ui-components-require-this-in-wpf
+            window.Closed += (s, e) => System.Windows.Threading.Dispatcher.ExitAllFrames();
+            System.Windows.Threading.Dispatcher.Run();
+        }
+
+        public static void StartGraphHostWindow(INetworkGraph Graph) {
+            Thread t = new Thread(RunGraphHostWindow);
+            t.SetApartmentState(ApartmentState.STA);
+            t.Start(Graph);
+        }
+    }
+}
