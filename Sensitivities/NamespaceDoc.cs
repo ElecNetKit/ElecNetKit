25c99d605e27bab765913899e8506da55759d60f
Fabian Tamp <fabian@capnfabs.net>
2013-01-23 13:30:41 +1100

    Documentation for Sensitivities module.

---
+++ Sensitivities/NamespaceDoc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElecNetKit.Sensitivities
+{
+    /// <summary>
+    /// The <see cref="ElecNetKit.Sensitivities"/> namespace defines a set of classes
+    /// for running perturb-and-observe style experiments, and for obtaining sensitivity
+    /// information from them.
+    /// </summary>
+    /// <remarks>For more information see <conceptualLink target="846f840c-9009-47f2-9838-a8f2f6998a0d" />.
+    ///</remarks>
+    [System.Runtime.CompilerServices.CompilerGenerated]
+    class NamespaceDoc
+    {
+    }
+}
